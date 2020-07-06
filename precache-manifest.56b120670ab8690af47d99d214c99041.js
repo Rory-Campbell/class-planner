75e41cb18257cb238953c26b5345c85dc152a034
Rory Campbell <rorycampbell@Rorys-MacBook-Air-2.local>
2020-07-06 20:28:45 +1000

    Updates

---
+++ precache-manifest.56b120670ab8690af47d99d214c99041.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7686a06112e18a1f59b080ef2a84d666",
+    "url": "/class-planner/index.html"
+  },
+  {
+    "revision": "6ed11e0aed006e677299",
+    "url": "/class-planner/static/css/main.7bc481e5.chunk.css"
+  },
+  {
+    "revision": "173702581c29c2dd3e1b",
+    "url": "/class-planner/static/js/2.3d29da19.chunk.js"
+  },
+  {
+    "revision": "6ed11e0aed006e677299",
+    "url": "/class-planner/static/js/main.524ec25c.chunk.js"
+  },
+  {
+    "revision": "548d9dcc6e71285a6322",
+    "url": "/class-planner/static/js/runtime-main.4bdc9ee5.js"
+  }
+]);
